f81ec6ca3c56ef59953e06f868ea2cafab18504c
bigdesire0210 <bigdesire210@gmail.com>
2020-12-29 19:07:03 +0900

    12/29　事前課題の更新

--- handson2/main.js
+++ handson2/main.js
@@ -1,14 +1,62 @@
-$(function () {
-    // ハンバーガーメニュー
-    $(".ham-btn").click(function () {
-        $(this).toggleClass("active");
-        if ($(this).hasClass("active")) {
-            $(".ham-menu").addClass("active");
-        } else {
-            $(".ham-menu").removeClass("active");
-        }
+// 'use strict';
+
+// {
+//     const open = document.getElementById('open')
+//     const overlay = document.querySelector('.ham')
+//     const close = document.getElementById('close')
+//     const mnclose = document.getElementById('mn-close')
+//     const cpclose = document.getElementById('cp-close')
+//     const acclose = document.getElementById('ac-close')
+//     const rsclose = document.getElementById('rs-close')
+
+
+//     open.addEventListener('click', () => {
+//         overlay.classList.add('show');
+//         open.classList.add('hide');
+//     });
+
+//     close.addEventListener('click', () => {
+//         overlay.classList.remove('show');
+//         open.classList.remove('hide');
+//     });
+
+//     mnclose.addEventListener('click', () => {
+//         overlay.classList.remove('show');
+//         open.classList.remove('hide');
+//     });
+
+//     cpclose.addEventListener('click', () => {
+//         overlay.classList.remove('show');
+//         open.classList.remove('hide');
+//     });
+
+//     acclose.addEventListener('click', () => {
+//         overlay.classList.remove('show');
+//         open.classList.remove('hide');
+//     });
+
+//     rsclose.addEventListener('click', () => {
+//         overlay.classList.remove('show');
+//         open.classList.remove('hide');
+//     });
+// }
+
+function hamburger() {
+    document.getElementById('line1').classList.toggle('linea');
+    document.getElementById('line2').classList.toggle('lineb');
+    document.getElementById('line3').classList.toggle('linec');
+    document.getElementById('target').classList.toggle('slidex');
+    document.getElementById('nav_f').classList.toggle('fadein');
+}
+
+document.getElementById('target').addEventListener('click', function () {
+    hamburger();
+});
+
+let list = document.getElementsByClassName('list');
+
+for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener('click', function () {
+        hamburger();
     });
-    $(".ham-list").click(function () {
-        $(".ham-menu,.ham-btn").removeClass("active");
-    });
-})
+}
